4de9b09991a95fd68d7358a2b8c42e858fdb72be
UPMANYU SHARMA <upmanyusharma31@gmail.com>
2023-04-30 15:27:57 +0530

    fixes

--- data/database.js
+++ data/database.js
@@ -1,11 +1,13 @@
+import dotenv from "dotenv";
 import mongoose from "mongoose";
+dotenv.config();
 
 export const connectDB = () => {
     mongoose
-        .connect(process.env.MONGO_URL, {
+        .connect(process.env.MONGO_URI, {
             DBname: "BackendAPI",
             useNewUrlParser: true,
         })
         .then(() => console.log("DataBase Connected"))
-        .catch((e) => console.log(e));
+        .catch((e) => console.log(e.message));
 };
